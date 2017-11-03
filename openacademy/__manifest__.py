6724ec2d39eb1cd9f29ff86913fb1720de05f06e
mgrenson <michel.grenson@unamur.be>
2017-11-03 16:45:17 +0100

    tests openacademy

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'report', 'board'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/course.xml',
+        'views/session.xml',
+        'views/partner.xml',
+        'views/session_board.xml',
+        'views/session_workflow.xml',
+        'views/views.xml',
+        'views/templates.xml',
+        'reports/sessions.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ openacademy/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import course
+from . import session
+from . import partner
+from . import wizard
